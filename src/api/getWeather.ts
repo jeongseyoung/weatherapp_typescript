06932e3911c1dd38419fa89256181b3f6d1970f1
jeongseyoung <sengangel@naver.com>
2025-06-01 23:50:27 +0900

    Update getWeather.ts

--- src/api/getWeather.ts
+++ src/api/getWeather.ts
@@ -13,7 +13,7 @@ export const getWeatherData = async (city: string) => {
         lat = result.lat;
         lon = result.lon;
       }
-    }); 
+    });   
     // 날씨 정보 가져오기
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openweatherkey}`;
     const result = await fetch(url).then((response) => response.json());

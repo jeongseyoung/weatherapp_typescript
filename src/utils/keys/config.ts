5b769c9c541d39c264d495bfb7e904e15bd463cf
jeongseyoung <sengangel@naver.com>
2026-08-01 23:55:22 +0900

    Update config.ts

--- src/utils/keys/config.ts
+++ src/utils/keys/config.ts
@@ -1,4 +1,4 @@
-// npm install dotenv
+// npm install dotenv// 데일리 기온list// 데일리 기온list
 export const openweatherkey = process.env.REACT_APP_openweatherAPI_key;
 export const windyAPIKey = process.env.REACT_APP_windyAPI_key;
 export const onecallAPIkey = process.env.REACT_APP_onecallAPI_key;
